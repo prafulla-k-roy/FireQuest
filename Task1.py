2dfe38d6a1857dd3ebc90eba38c1d5cc980eb183
prafulla-k-roy <69381612+prafulla-k-roy@users.noreply.github.com>
2021-07-12 14:20:39 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,18 +1,30 @@
+'''
+Team name: Peace
+Team members: 
+-> Prafulla Kumar Roy (Prafulla Kumar Roy#4108)
+-> Utkarsh Saurav (utkarsh_saurav#5287)
+-> Vaibhav Mani (demon_28f#2042)
+'''
+
 import requests
 
+# Accessing Data form API
 response = requests.get("https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22")
 
+# Converting the data in string form to Dictionary
 w_data = response.json()
 
+# The data we actually need lies in this list
 lst = w_data['list']
 
+# Getting user's choice
 ch = int(input("1. Get weather\n2. Get Wind Speed\n3. Get Pressure\n0. Exit\n"))
 
 if ch == 1:
     date = input("Enter date (YYYY-MM-DD): ")
     for i in range(0, len(lst)):
         weather_data = lst[i]
-        date_time = weather_data['dt_txt'].split(' ')
+        date_time = weather_data['dt_txt'].split(' ')   # Getting date and time
         if date == date_time[0]:
             w = weather_data['weather']
             weather = w[0]
@@ -23,7 +35,7 @@ elif ch == 2:
     date = input("Enter date (YYYY-MM-DD): ")
     for i in range(0, len(lst)):
         weather_data = lst[i]
-        date_time = weather_data['dt_txt'].split(' ')
+        date_time = weather_data['dt_txt'].split(' ')   # Getting date and time
         if date == date_time[0]:
             wind = weather_data['wind']
             print("Wind Speed on ", date, " at time ", date_time[1], " is ", wind['speed'], " miles per hour.")
@@ -33,7 +45,7 @@ elif ch == 3:
     date = input("Enter date (YYYY-MM-DD): ")
     for i in range(0, len(lst)):
         weather_data = lst[i]
-        date_time = weather_data['dt_txt'].split(' ')
+        date_time = weather_data['dt_txt'].split(' ')   # Getting date and time
         if date == date_time[0]:
             t_p = weather_data['main']
             print("Air pressure on ", date, " at time ", date_time[1], " is ", t_p['pressure'], " Pascal.")
